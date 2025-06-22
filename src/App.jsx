82eda173a9dd75a627fdd831a98e709c6fcbd852
faijalfshaikh <faijalfshaikh@gmail.com>
2025-06-22 13:10:49 +0530

    Added Buttons with functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import { useState } from "react"
+
 function App() {
 
-  return <div className="fixed flex w-full h-screen flex-wrap" style={{backgroundColor:"green"}}>
+  let [color,setColor] = useState("olive");
 
+  return <div className="fixed flex w-full h-screen flex-wrap" style={{backgroundColor:color}}>
+
+    <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+
+    <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-full">
+      
+      <button
+      className="outline-none px-4 py-2 rounded-full" style={{backgroundColor:"red",color:"white"}}
+      onClick={()=>setColor('red')}
+      >Red</button>
+
+      <button
+      className="outline-none px-4 py-2 rounded-full" style={{backgroundColor:"green",color:"white"}}
+      onClick={()=>setColor('green')}
+      >Green</button>
+
+      <button
+      className="outline-none px-4 py-2 rounded-full" style={{backgroundColor:"blue",color:"white"}}
+      onClick={()=>setColor('blue')}
+      >Blue</button>
+    </div>
+    </div>
   </div>
 }
 
